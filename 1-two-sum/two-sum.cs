532321c082c482b7447c66f78c950298f81745bd
Huy Ho <147988746+huyho255@users.noreply.github.com>
2026-04-25 22:14:51 +0700

    Time: 4 ms (84.95%) | Memory: 50 MB (29.24%) - LeetSync

--- 1-two-sum/two-sum.cs
+++ 1-two-sum/two-sum.cs
@@ -2,8 +2,6 @@ using System.Collections.Generic;
 
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        // Dictionary to store the number we've seen as the key, 
-        // and its index as the value.
         Dictionary<int, int> seen = new Dictionary<int, int>();
         
         for (int i = 0; i < nums.Length; i++) {
@@ -13,15 +11,8 @@ public class Solution {
             if (seen.ContainsKey(complement)) {
                 return new int[] { seen[complement], i };
             }
-            
-            // Otherwise, add the current number and its index to the dictionary.
-            // Using the indexer [] handles potential duplicate values safely 
-            // by updating the index to the most recent one.
             seen[nums[i]] = i;
         }
-        
-        // The problem states there is exactly one valid solution, 
-        // so we will always return inside the loop.
         return new int[0];
     }
 }
